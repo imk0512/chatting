29cecee81b6e11aa0750b4300d69100c5808301e
imk0512 <imk3626@gmail.com>
2023-10-26 14:18:47 +0900

    initial commit

---
+++ chatting/chattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  chattingApp.swift
+//  chatting
+//
+//  Created by 緒方亮平 on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct chattingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
